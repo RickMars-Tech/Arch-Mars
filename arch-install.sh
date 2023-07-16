6427de1b9bba97ebfa45b82636f5e5e10b5a4b42
RickMars <139187404+RickMars-Tech@users.noreply.github.com>
2023-07-16 14:21:05 -0600

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -43,9 +43,18 @@ else
   fi
 fi
 
+# Agregar repo de Chaotic-Aur
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+echo -e "[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist " sudo tee -a /etc/pacman.conf > /dev/null
+echo -e "Se ha configurado Chaotic-Aur"
+
 # Actualizar el sistema antes de proceder
 printf "${YELLOW} Actualización del sistema para evitar problemas\n"
-yay -Syu --noconfirm 2>&1 | tee -a "$LOG"
+sudo pacman -Sy --noconfirm && sudo powerpill -Su --noconfirm && paru -Su --noconfirm 2>&1 | tee -a "$LOG"
 
 # Activar Repositorio de Flatpak
 pacman -S flatpak
