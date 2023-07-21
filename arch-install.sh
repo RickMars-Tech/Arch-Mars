244eea84c419f498ca80f36f5008de98319dee3d
RickMars <139187404+RickMars-Tech@users.noreply.github.com>
2023-07-20 22:31:32 -0600

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -64,11 +64,11 @@ sudo flatpak install -y net.davidotek.pupgui2 org.mamedev.MAME \
 # Instalacion de herramientas
 echo "Instalando Herramientas y Aplicaciones"
 paru -S archlinux-tweak-tool-git bashtop-git eww-wayland gtklock heroic-games-launcher hyprpicker-git \
-    pamac-aur powerpill playerctl-git sddm stacer-bin timeshift-bin otf-font-awesome \
-    nerd-fonts-sf-mono otf-nerd-fonts-monacob-mono gotop-bin acpi adobe-source-han-sans-jp-fonts \
+    pamac-aur powerpill playerctl-git sddm sddm-sugar-dark sddm-sugar-candy-git sddm-elegant-theme-git \
+    otf-font-awesome nerd-fonts-sf-mono otf-nerd-fonts-monacob-mono gotop-bin acpi adobe-source-han-sans-jp-fonts \
     asusctl alsa-lib adobe-source-han-sans-kr-fonts ttf-jetbrains-mono-nerd ttf-jetbrains-mono \
-    alsa-plugins bat bluez brightnessctl cups dunst ffmpeg ffmpegthumbnailer firefox \
-    gamemode gamescope-plus gedit giflib gnome-bluetooth-3.0 gnome-disk-utility gnutls gjs \
+    stacer-bin timeshift-bin alsa-plugins bat bluez brightnessctl cups dunst ffmpeg ffmpegthumbnailer \
+    gamemode gamescope-plus gedit giflib gnome-bluetooth-3.0 gnome-disk-utility gnutls gjs firefox \
     gimp grim gst-plugins-base-libs gtk3 hyprland hyprpaper imv inotify-tools jdk-openjdk \
     jq kitty kvantum lib32-alsa-lib lib32-alsa-plugins lib32-giflib lib32-gnutls \
     qt5ct lib32-gamemode lib32-gst-plugins-base-libs lib32-libjpeg-turbo \
