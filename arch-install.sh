461969adb6924dbac46b8d7979f42f7ed164da69
RickMars <139187404+RickMars-Tech@users.noreply.github.com>
2023-07-25 14:49:53 -0600

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -102,7 +102,7 @@ cp -r dotconfig/config/* ~/.config/ 2>&1 | tee -a "$LOG"
 
 printf " Copiando archivos extra...\n"
 cp -r applications/ ~/.local/share/ 2>&1 | tee -a "$LOG"
-cp -r wal/ ~/ 2>&1 | tee -a "$LOG"
+cp -r dotconfig/wal/ ~/ 2>&1 | tee -a "$LOG"
 cp -r SKLauncher/ ~/ 2>&1 | tee -a "$LOG"
 
 printf " Dando permisos a archivos...\n"
